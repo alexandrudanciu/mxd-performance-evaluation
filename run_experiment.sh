eb7c4110ce221a112ada80cca4c28dfde7f88d85
JanisKretschmann <janis.kretschmann@sap.com>
2024-01-03 15:56:15 +0100

    Added properties file to the execution of the .jmx-files

--- run_experiment.sh
+++ run_experiment.sh
@@ -10,10 +10,6 @@ echo "Jmeter found at path $JMETER_HOME"
 
 chmod +x run_experiment.sh
 
-transfer_properties() {
-  source experiment.properties
-}
-
 jmeter_files=("setup.jmx" "measurement_interval.jmx" "tear_down.jmx")
 
 jmeter_bin="$JMETER_HOME/bin/jmeter"
@@ -22,6 +18,5 @@ jmeter_bin="$JMETER_HOME/bin/jmeter"
 for file in "${jmeter_files[@]}"
 do
     echo "Executing JMeter script: $file"
-    transfer_properties "$file"
     $jmeter_bin -n -t $file -l "test_summary.json" -q experiment.properties
 done
