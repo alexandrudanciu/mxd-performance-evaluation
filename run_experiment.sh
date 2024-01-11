61fd1c7b98957af20f07fceec91b44016ae7efca
hemantxpatel <pratapi.hemant.patel@sap.com>
2024-01-11 11:56:20 +0530

    Update run experiment script with options

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,72 +1,84 @@
 #!/bin/bash
+###############################################################################
+# Examples:                                                                   #
+#   ./run_experiment.sh                                                       #
+#   ./run_experiment.sh -t M                                                  #
+#   ./run_experiment.sh -q L                                                  #
+#   ./run_experiment.sh -t T -q S                                             #
+#                                                                             #
+# Command Line Options:                                                       #
+# -t : Jmeter Script to execute [Optional]                                    #
+#      Options:                                                               #
+#        S -> setup.jmx                                                       #
+#        M -> measurement_interval.jmx                                        #
+#        T -> tear_down.jmx                                                   #
+#      Default:                                                               #
+#        Execute all scripts in order S, M and T.                             #
+#                                                                             #
+# -q : Jmeter Properties [Optional]                                           #
+#      Options:                                                               #
+#        S -> small_experiment.properties                                     #
+#        M -> medium_experiment.properties                                    #
+#        L -> large_experiment.properties                                     #
+#      Default:                                                               #
+#        S                                                                    #
+#                                                                             #
+###############################################################################
 
-if [ -z "$JMETER_HOME" ]; 
-then
+declare -A jmeter_scripts=( ["S"]="setup.jmx"
+                            ["M"]="measurement_interval.jmx"
+                            ["T"]="tear_down.jmx")
+
+declare -A properties_map=( ["S"]="small_experiment.properties"
+                            ["M"]="medium_experiment.properties"
+                            ["L"]="large_experiment.properties")
+
+jmeter_script_opt=""
+experiment_property_opt="S"
+
+while getopts t:q: flag
+do
+    case "${flag}" in
+        t) jmeter_script_opt=${OPTARG};;
+        q) experiment_property_opt=${OPTARG};;
+    esac
+done
+
+jmeter_script=""
+if [ -n "$jmeter_script_opt" ]; then
+  jmeter_script="${jmeter_scripts[$jmeter_script_opt]}"
+fi
+experiment_property="${properties_map[$experiment_property_opt]}"
+
+if [ -z "$JMETER_HOME" ]; then
   echo "JMETER_HOME is not defined - please define it."
   exit 1  
 fi
-
 echo "Jmeter found at path $JMETER_HOME"
-chmod +x run_experiment.sh
-jmeter_bin="$JMETER_HOME/bin/jmeter"
 
-jmeter_files=("setup.jmx" "measurement_interval.jmx" "tear_down.jmx")
+jmeter_binary="$JMETER_HOME/bin/jmeter"
 
-if [ -z "$1" ]; then
-    echo "Please define the scope of the test execution by specifying the .properties file (S = small_experiment.properties, M = medium_experiement.properties and L = large_experiment.properties)"
-    exit 1
-fi
+rm -rf output
+mkdir -p output/dashboard
 
-if [ -z "$2" ]; then
-    echo "Please define the phase of the test execution by specifying the JMeter script (S = setup.jmx, M = measurement_interval.jmx, T = tear_down.jmx and SMT = Sequential execution of all scripts)"
-    exit 1
-fi
+echo "*** Property: $experiment_property Start ***"
+cat $experiment_property
+echo "*** Property: $experiment_property End ***"
 
-if [ "$1" = "S" ] && [ "$2" = "S" ]; then
-    $jmeter_bin -n -t setup.jmx -l test_summary.jtl -q small_experiment.properties -e -o report
-    echo "Executing setup.jmx with the properties of small_experiment.properties"
-elif [ "$1" = "S" ] && [ "$2" = "M" ]; then
-    $jmeter_bin -n -t measurement_interval.jmx -l test_summary.jtl -q small_experiment.properties -e -o report
-    echo "Executing measurement.jmx with the properties of small_experiment.properties"
-elif [ "$1" = "S" ] && [ "$2" = "T" ]; then
-    $jmeter_bin -n -t tear_down.jmx -l test_summary.jtl -q small_experiment.properties -e -o report
-    echo "Executing tear_down.jmx with the properties of small_experiment.properties"
-elif [ "$1" = "S" ] && [ "$2" = "SMT" ]; then
-    for file in "${jmeter_files[@]}"
-    do
-        echo "Executing JMeter script: $file"
-        $jmeter_bin -n -t $file -l test_summary.jtl -q medium_experiment.properties -e -o report
-    done
-elif [ "$1" = "M" ] && [ "$2" = "S" ]; then
-    $jmeter_bin -n -t setup.jmx -l test_summary.jtl -q medium_experiment.properties -e -o report
-    echo "Executing setup.jmx with the properties of medium_experiment.properties"
-elif [ "$1" = "M" ] && [ "$2" = "M" ]; then
-    $jmeter_bin -n -t measurement_interval.jmx -l test_summary.jtl -q medium_experiment.properties -e -o report
-    echo "Executing measurement.jmx with the properties of medium_experiment.properties"
-elif [ "$1" = "M" ] && [ "$2" = "T" ]; then
-    $jmeter_bin -n -t tear_down.jmx -l test_summary.jtl -q medium_experiment.properties -e -o report
-    echo "Executing tear_down.jmx with the properties of medium_experiment.properties"
-elif [ "$1" = "M" ] && [ "$2" = "SMT" ]; then
-    for file in "${jmeter_files[@]}"
-    do
-        echo "Executing JMeter script: $file with the properties of medium_experiment.properties"
-        $jmeter_bin -n -t $file -l test_summary.jtl -q medium_experiment.properties -e -o report
-    done
-elif [ "$1" = "L" ] && [ "$2" = "S" ]; then
-    $jmeter_bin -n -t setup.jmx -l test_summary.jtl -q large_experiment.properties -e -o report
-    echo "Executing setup.jmx with the properties of large_experiment.properties"
-elif [ "$1" = "L" ] && [ "$2" = "M" ]; then
-    $jmeter_bin -n -t measurement_interval.jmx -l test_summary.jtl -q large_experiment.properties -e -o report
-    echo "Executing measurement.jmx with the properties of large_experiment.properties"
-elif [ "$1" = "L" ] && [ "$2" = "T" ]; then
-    $jmeter_bin -n -t tear_down.jmx -l test_summary.jtl -q large_experiment.properties -e -o report
-    echo "Executing tear_down.jmx with the properties of large_experiment.properties"
-elif [ "$1" = "L" ] && [ "$2" = "SMT" ]; then
-    for file in "${jmeter_files[@]}"
-    do
-        echo "Executing JMeter script: $file with the properties of large_experiment.properties"
-        $jmeter_bin -n -t $file -l test_summary.jtl -q large_experiment.properties -e -o report
-    done
+echo "*** Performance Test Start ***"
+if [ -z "$jmeter_script" ]; then
+  echo "Executing all scripts in order S, M && T"
+  $jmeter_binary -n -t setup.jmx -l output/setup.jtl -q experiment_property
+  $jmeter_binary -n -t measurement_interval.jmx -l output/measurement_interval.jtl -q experiment_property -e -o output/dashboard
+  $jmeter_binary -n -t tear_down.jmx -l output/tear_down.jtl -q experiment_property
 else
-    echo "Please enter valid values!"
+  echo "Executing script: $jmeter_script"
+  $jmeter_binary -n -t $jmeter_script -l output/measurement_interval.jtl -q experiment_property -e -o output/dashboard
 fi
+echo "*** Performance Test End ***"
+
+echo "Creating tar file with output"
+tar -cf output.tar output
+
+echo "*** Test Completed..., Sleeping now ***"
+while true; do sleep 10000; done
