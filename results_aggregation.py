aa5c4805e7acd691aafbe5449d3c784ff9ee53c9
JanisKretschmann <janis.kretschmann@sap.com>
2024-02-21 11:57:32 +0100

    Added cursor

--- results_aggregation.py
+++ results_aggregation.py
@@ -19,7 +19,11 @@ def extract_values(file_path):
         if 'OEM_PLANTS' in line:
             values['OEM_PLANTS'] = int(line.split('=')[1].strip())
         elif 'OEM_CARS_INITIAL' in line:
-            values['OEM_CARS_INITIAL'] = line.split('=')[1].strip()
+            values['OEM_CARS_INITIAL'] = int(line.split('=')[1].strip())
+        elif 'PARTS_PER_CAR' in line:
+            values['PARTS_PER_CAR'] = int(line.split('=')[1].strip())
+        elif 'CARS_PRODUCED_PER_INTERVALL' in line:
+            values['CARS_PRODUCED_PER_INTERVALL'] = int(line.split('=')[1].strip())
     return values
 
 # List comprehension to gather all directories under the specified root_folder
@@ -34,6 +38,7 @@ for action_to_consider in processes:
 
         stats = []
         labels = []
+        meta_values_list = []
 
         # Goes through each directory to find the metadata and statistics files
         for idx, directory in enumerate(directories):
@@ -48,18 +53,30 @@ for action_to_consider in processes:
                 with open(statistics_path, 'r') as f:
                     stats_data = json.load(f)
 
-                # appends the statistics for each action and the associated labels for the plot
+                # Appends the statistics for each action and the associated labels for the plot
                 action = stats_data[action_to_consider]
                 stats.append((meta_values['OEM_PLANTS'], action[value]))
-                labels.append('Plants: {}, Cars: {}'.format(meta_values['OEM_PLANTS'], meta_values['OEM_CARS_INITIAL']))
+                labels.append('Plants: {}, {}: {:.2f}'.format(meta_values['OEM_PLANTS'], value, round(action[value],2)))
+                meta_values_list.append(meta_values)
 
-        # plots the statistics data as a line plot and a scatter plot
+        # Plots the statistics data as a line plot and a scatter plot
         plt.plot([s[0] for s in stats], [s[1] for s in stats])
-        plt.scatter([s[0] for s in stats], [s[1] for s in stats], color='red')
+        scatter = plt.scatter([s[0] for s in stats], [s[1] for s in stats], color='red')
 
-        # adds labels to each point on the scatter plot (that x-axis matches OEM_PLANTS values)
+        # Adds labels to each point on the scatter plot (that x-axis matches OEM_PLANTS values)
         for i, text in enumerate(labels):
             plt.text(stats[i][0], stats[i][1], text)
+
+        # Add cursor
+        cursor = mplcursors.cursor(scatter, hover=True)
+        @cursor.connect("add")
+        def on_add(sel):
+            i = sel.target.index
+            sel.annotation.set_text('Plants: {}, Cars: {}, Parts/Car: {}, Cars/Interval: {}'.format(
+                stats[i][0],
+                meta_values_list[i]['OEM_CARS_INITIAL'],
+                meta_values_list[i]['PARTS_PER_CAR'],
+                meta_values_list[i]['CARS_PRODUCED_PER_INTERVALL']))
 
         plt.xlabel('OEM_PLANTS')
         plt.ylabel(value)
