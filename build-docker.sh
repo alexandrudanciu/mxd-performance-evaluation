be32f7e86b7fd8a0faf029e979c2fd88fcde2679
ciprian-nicuta <ciprian.nicuta@sap.com>
2024-02-23 11:50:42 +0200

    docker image placeholder

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-docker build -t ciprian2398/mxd-performance-test:0.1.1-SNAPSHOT .
+docker build -t PLACEHOLDER/mxd-performance-test:0.1.2-SNAPSHOT .
 
-docker image push ciprian2398/mxd-performance-test:0.1.1-SNAPSHOT
+docker image push PLACEHOLDER/mxd-performance-test:0.1.2-SNAPSHOT
